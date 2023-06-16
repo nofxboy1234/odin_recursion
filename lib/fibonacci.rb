90fc73daab259ea07876545de62da01fc7960758
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-16 16:58:36 +0200

    Make test pass for fibs(8)

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,3 +1,18 @@
-def fibs(count)
-  
+def fibs(max_count)
+  # [0, 1, 1, 2, 3, 5, 8, 13]
+  fib = []
+  (0..max_count - 1).each do |num|
+    if [0, 1].include?(num)
+      fib << num
+    else
+      fib << [fib[-2], fib[-1]].sum
+    end
+  end
+  fib
+end
+
+def fib_value(n, count = 0, current_next = [0, 1])
+  return current_next[0] if n == count
+
+  fib_value(n, count + 1, [current_next[1], current_next[0] + current_next[1]])
 end
