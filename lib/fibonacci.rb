13886a09f72915f86e69e7d83782ac2555b80a60
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-17 13:27:29 +0200

    Update conditional structure in #fibs

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,25 +1,25 @@
 def fibs(max_count)
   fib = []
   (0..max_count - 1).each do |num|
-    if [0, 1].include?(num)
-      fib << num
-    else
-      fib << [fib[-2], fib[-1]].sum
-    end
+    fib << if [0, 1].include?(num)
+             num
+           else
+             [fib[-2], fib[-1]].sum
+           end
   end
   fib
 end
 
-def fibs_rec(n, count = 0, current_next = [0, 1])
+def fibs_rec(n)
+  # base case: when
+
   return current_next[0] if n == count
 
   fibs_rec(n, count + 1, [current_next[1], current_next[0] + current_next[1]])
 end
-
 
 def fibs_value_at(n, count = 0, current_next = [0, 1])
   return current_next[0] if n == count
 
   fibs_value_at(n, count + 1, [current_next[1], current_next[0] + current_next[1]])
 end
-
