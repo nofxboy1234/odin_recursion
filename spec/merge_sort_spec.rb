51b27984ae6e21f6b0e51c08510fcebae633e5b8
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-19 14:45:03 +0200

    Re-arrange tests

--- spec/merge_sort_spec.rb
+++ spec/merge_sort_spec.rb
@@ -20,6 +20,24 @@ RSpec.describe 'MergeSort' do
       end
     end
 
+    context 'when the array has the 2 middle numbers of the first left half sorted' do
+      it 'returns a sorted array' do
+        expect(merge_sort([4, 6, 8, 2, 1, 7, 5, 3])).to eq([1, 2, 3, 4, 5, 6, 7, 8])
+      end
+    end
+
+    context 'when the array has the 2 rightmost numbers of the first left half sorted' do
+      it 'returns a sorted array' do
+        expect(merge_sort([4, 8, 2, 6, 1, 7, 5, 3])).to eq([1, 2, 3, 4, 5, 6, 7, 8])
+      end
+    end
+
+    context 'when the array is already sorted' do
+      it 'returns a sorted array' do
+        expect(merge_sort([1, 2, 3, 4, 5, 6, 7, 8])).to eq([1, 2, 3, 4, 5, 6, 7, 8])
+      end
+    end
+
     context 'when the array has left half sorted' do
       it 'returns a sorted array' do
         expect(merge_sort([2, 4, 6, 8, 1, 7, 5, 3])).to eq([1, 2, 3, 4, 5, 6, 7, 8])
@@ -43,12 +61,5 @@ RSpec.describe 'MergeSort' do
         expect(merge_sort([8, 7, 6, 5, 4, 3, 2, 1])).to eq([1, 2, 3, 4, 5, 6, 7, 8])
       end
     end
-
-    context 'when the array is already sorted' do
-      it 'returns a sorted array' do
-        expect(merge_sort([1, 2, 3, 4, 5, 6, 7, 8])).to eq([1, 2, 3, 4, 5, 6, 7, 8])
-      end
-    end
-
   end
 end
